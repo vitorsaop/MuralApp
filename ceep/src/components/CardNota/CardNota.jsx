84026c7c549e1ca3826828cfa69d1e8515d13b6d
Vitor Oliveira <vitorsaop@gmail.com>
2021-06-07 00:12:00 -0300

    fifth commit of project part 5

--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import "./estilo.css"
-import "./index.js"
+import "./estilo.css";
+import "./index.js";
 
 class CardNota extends Component {
   state = {};
@@ -8,9 +8,9 @@ class CardNota extends Component {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
-          <h3 className="card-nota_titulo">Título</h3>
+          <h3 className="card-nota_titulo">{this.props.titulo}</h3>
         </header>
-        <p className="card-nota_texto">Escreva sua Nota</p>
+        <p className="card-nota_texto">{this.props.texto}</p>
       </section>
     );
   }
--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -3,13 +3,14 @@ import CardNota from "../CardNota";
 import "./estilo.css";
 
 class ListaDeNotas extends Component {
+
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("Trabalho", "Trabalho", "Estudo").map((categoria, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota />
+              <CardNota titulo={nota.titulo} texto={nota.texto}/>
             </li>
           );
         })}
