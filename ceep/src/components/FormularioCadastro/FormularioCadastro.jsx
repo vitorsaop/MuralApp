521564359290e12f89e6ca55f5713ea0d098af09
Vitor Oliveira <vitorsaop@gmail.com>
2021-06-09 19:16:39 -0300

    last commit of project

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-
   constructor(props) {
     super(props);
     this.titulo = "";
     this.texto = "";
+    this.categoria = "Sem Categoria";
+    this.state = {categorias:[]}
+    this._novasCategorias =  this._novasCategorias.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount(){
+    this.props.categorias.desinscrever(this._novasCategorias);
+  }
+
+  _novasCategorias(categorias) {
+    this.setState({...this.state, categorias})
   }
 
   _handleMudancaTitulo(evento) {
@@ -18,15 +32,26 @@ class FormularioCadastro extends Component {
     this.texto = evento.target.value;
   }
 
+  _handleMudancaCategoria(evento) {
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
+  }
+
   _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
   }
 
   render() {
     return (
       <form className="form-cadastro " onSubmit={this._criarNota.bind(this)}>
+        <select onChange={this._handleMudancaCategoria.bind(this)} className="form-cadastro_input">
+          <option>Sem Categoria</option>
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria}</option>;
+          })}
+        </select>
         <input
           type="text"
           placeholder="Título"
--- ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -2,12 +2,32 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class ListaDeCategorias extends Component {
-  //state = {  }
+ 
+  constructor(){
+    super();
+    this.state = {categorias:[]}
+    this._novasCategorias = this._novasCategorias.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+  
+  componentWillUnmount(){
+    this.props.categorias.desinscrever(this._novasCategorias);
+  }
+
+
+  _novasCategorias(categorias){
+    //console.log(categorias)
+    this.setState({...this.state,categorias})
+  }
 
   _handleEventoInput(e) {
     if (e.key == "Enter") {
       let valorCategoria = e.target.value;
       this.props.adicionarCategoria(valorCategoria);
+      this.props.categorias.inscrever();
     }
   }
 
@@ -15,7 +35,7 @@ class ListaDeCategorias extends Component {
     return (
       <section className="lista-categorias">
         <ul className="lista-categorias_lista">
-          {this.props.categorias.map((categoria, index) => {
+          {this.state.categorias.map((categoria, index) => {
             return (
               <li key={index} className="lista-categorias_item">
                 {categoria}
